b6f5304b3570ea4d203905a1a495751c69415d34
Araceli Martin <araceli.martin@globant.com>
2018-05-31 11:03:50 -0300

    Initial commit upload

---
+++ assets/js/scripts.js
@@ -0,0 +1,60 @@
+jQuery(document).ready(function() {
+
+    /*
+   
+        Countdown initializer
+    */
+    var now = new Date();
+    var countTo = 895 * 24 * 60 * 60 * 1000 + now.valueOf();
+    $('.timer').countdown(countTo, function(event) {
+        var $this = $(this);
+        switch(event.type) {
+            case "seconds":
+            case "minutes":
+            case "hours":
+            case "days":
+            case "weeks":
+            case "daysLeft":
+                $this.find('span.'+event.type).html(event.value);
+                break;
+            case "finished":
+                $this.hide();
+                break;
+        }
+    });
+
+    /*
+
+        Subscription form
+    */
+    $('.success-message').hide();
+    $('.error-message').hide();
+
+    $('.subscribe form').submit(function() {
+        var postdata = $('.subscribe form').serialize();
+        $.ajax({
+            type: 'POST',
+            url: 'assets/sendmail.php',
+            data: postdata,
+            dataType: 'json',
+            success: function(json) {
+                if(json.valid == 0) {
+                    $('.success-message').hide();
+                    $('.error-message').hide();
+                    $('.error-message').html(json.message);
+                    $('.error-message').fadeIn();
+                }
+                else {
+                    $('.error-message').hide();
+                    $('.success-message').hide();
+                    $('.subscribe form').hide();
+                    $('.success-message').html(json.message);
+                    $('.success-message').fadeIn();
+                }
+            }
+        });
+        return false;
+    });
+
+});
+
